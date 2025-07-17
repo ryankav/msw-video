2d32912cb2c3935e446861c41a3e64bb959828fb
ryankav <ryan.p.kav@gmail.com>
2025-07-17 09:17:09 +0100

    Adding in partial content response

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import "./App.css";
 import { useState } from "react";
+import { Root, Thumb } from "@radix-ui/react-switch";
 
 function App() {
   const [handlerToggle, setHandlerToggle] = useState(false);
 
-  return <video src="/simple-example" controls />;
+  return (
+    <>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <label
+          className="Label"
+          htmlFor="partial-content-src"
+          style={{ paddingRight: 15 }}
+        >
+          Partial content source
+        </label>
+        <Root
+          checked={handlerToggle}
+          onCheckedChange={() => setHandlerToggle((prev) => !prev)}
+          className="SwitchRoot"
+          id="partial-content-src"
+        >
+          <Thumb className="SwitchThumb" />
+        </Root>
+      </div>
+      {handlerToggle ? (
+        <video src="/example" crossOrigin="anonymous" controls />
+      ) : (
+        <video src="/simple-example" controls />
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -11,7 +11,7 @@ const simpleFlowerHandler = http.get("/simple-example", async ({ request }) => {
   });
 });
 
-const rangeFlowerOptions = http.options("example", () => {
+const rangeFlowerOptions = http.options("/example", () => {
   return new HttpResponse(null, {
     status: 200,
     headers: {
@@ -22,14 +22,41 @@ const rangeFlowerOptions = http.options("example", () => {
   });
 });
 
+//const rangeFlowerHandler = http.get("/example", async ({ request }) => {
+//  console.log(request.headers.get("Range"));
+//  const easyVideo = await fetch("/flower.mp4").then((response) =>
+//    response.arrayBuffer(),
+//  );
+//
+//  return HttpResponse.arrayBuffer(easyVideo, {
+//    headers: { "content-type": "video/mp4" },
+//  });
+//});
+
 const rangeFlowerHandler = http.get("/example", async ({ request }) => {
-  console.log(request.headers.get("Range"));
   const easyVideo = await fetch("/flower.mp4").then((response) =>
     response.arrayBuffer(),
   );
 
-  return HttpResponse.arrayBuffer(easyVideo, {
-    headers: { "content-type": "video/mp4" },
+  const range = request.headers.get("Range");
+
+  if (!range) {
+    throw HttpResponse.text("Missing Range", { status: 400 });
+  }
+
+  const ranges = range.replace(/bytes=/, "").split("-");
+  const start = +ranges[0];
+  const end = ranges[1] ? +ranges[1] : easyVideo.byteLength - 1;
+  const content = easyVideo.slice(start, end);
+
+  return HttpResponse.arrayBuffer(content, {
+    status: 206,
+    headers: {
+      "Accept-Range": "bytes",
+      "Content-Range": `bytes=${start}-${end}/${easyVideo.byteLength}`,
+      "Content-Length": content.byteLength.toString(),
+      "Content-Type": "text/plain",
+    },
   });
 });
 
