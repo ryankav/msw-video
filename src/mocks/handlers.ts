cd658c4647984481f099cd949652f5a32e63161c
ryankav <ryan.p.kav@gmail.com>
2025-07-22 09:32:13 +0100

    Adding allow access header and option mock

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -22,7 +22,21 @@ const simpleFlowerHandler = http.get("/simple-example", async ({ request }) => {
 //  });
 //});
 
+const rangeFlowerOptions = http.options("*", () => {
+  console.log("option called");
+  return new HttpResponse(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Expose-Headers": "*",
+    },
+  });
+});
+
 const rangeFlowerHandler = http.get("/example", async ({ request }) => {
+  console.log(request.mode);
   const easyVideo = await fetch("/flower.mp4").then((response) =>
     response.arrayBuffer(),
   );
@@ -36,11 +50,12 @@ const rangeFlowerHandler = http.get("/example", async ({ request }) => {
   const ranges = range.replace(/bytes=/, "").split("-");
   const start = +ranges[0];
   const end = ranges[1] ? +ranges[1] : easyVideo.byteLength - 1;
-  const content = easyVideo.slice(start, end);
+  const content = easyVideo.slice(start, end + 1);
 
   return HttpResponse.arrayBuffer(content, {
     status: 206,
     headers: {
+      "Access-Control-Allow-Origin": "*",
       "Accept-Range": "bytes",
       "Content-Range": `bytes=${start}-${end}/${easyVideo.byteLength}`,
       "Content-Length": content.byteLength.toString(),
@@ -49,4 +64,8 @@ const rangeFlowerHandler = http.get("/example", async ({ request }) => {
   });
 });
 
-export const handlers = [simpleFlowerHandler, rangeFlowerHandler];
+export const handlers = [
+  simpleFlowerHandler,
+  rangeFlowerOptions,
+  rangeFlowerHandler,
+];
